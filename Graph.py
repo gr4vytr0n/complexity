006a960cb7eaa8097b18d5e254b96fd83aff2d48
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-16 23:22:21 -0800

    added methods to Graph.py

--- Graph.py
+++ Graph.py
@@ -1,3 +1,5 @@
+from Edge import Edge
+
 # undirected graph implementation
 class Graph(dict):
   def __init__(self, vs=[], es=[]):
@@ -15,9 +17,9 @@ class Graph(dict):
   def add_edge(self, e):
     """add (e) to the graph by adding an entry in both directions.
        if there is already an edge connecting these vertices, the new edge replaces it"""
-    v, w = e
-    self[v][w] = e
-    self[w][v] = e
+    a, b = e
+    self[a][b] = e
+    self[b][a] = e
 
   def get_edge(self, node1, node2):
     try:
@@ -30,3 +32,21 @@ class Graph(dict):
     v1, v2 = edge
     del self[v1][v2]
     del self[v2][v1]
+
+  def vertices(self):
+    return [v for v in self]
+
+  def edges(self):
+    return list(set(self[v][es] for v in self for es in self[v]))
+
+  def out_vertices(self, vertex):
+    return [ov for ov in self[vertex]]
+
+  def out_edges(self, vertex):
+    return [self[vertex][x] for x in self[vertex]]
+
+  def add_all_edges(self):
+    for v1 in self:
+      for v2 in self:
+        if v1 != v2:
+          self.add_edge(Edge(v1, v2))
--- graphing.py
+++ graphing.py
@@ -2,10 +2,18 @@ from Graph import Graph
 from Vertex import Vertex
 from Edge import Edge
 
+t = Vertex('t')
 v = Vertex('v')
 w = Vertex('w')
-#e = Edge(v, w)
+# e = Edge(v, w)
+# f = Edge(v, t)
 
-g = Graph([v, w])
+g = Graph([v, w, t])#, [e, f])
 
-
+#print(g)
+#print(g.vertices())
+g.add_all_edges()
+print(g.edges())
+# print(g.out_vertices(v))
+# print(g.out_edges(v))
+#print(g['Vertex("t")'])
