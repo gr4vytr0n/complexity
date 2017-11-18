53749213d1772bd3b813e6e22bed8c0c802fad87
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-17 17:53:39 -0800

    work on add_regular_graph method

--- Graph.py
+++ Graph.py
@@ -51,5 +51,46 @@ class Graph(dict):
         if v1 != v2:
           self.add_edge(Edge(v1, v2))
 
+  # Rules for building regular graphs
+    # -Preconditions: 0 < k and k < n
+    # -If n is odd, k must be even
+    # -If n is odd, we add edges between vertices x positions away,
+    #  where x is all odd numbers between 1 to k.
+    # -If n is even, and k is even, we add edges between vertices x
+    #  positions away, where x is a range of numbers starting from 1 to n/2,
+    #  and the range of these numbers is limited by the number of even numbers
+    #  from 1 to k, including k
+    # -If n is even, and k is odd, we add edges between vertices x positions away,
+    #  where x is a range of numbers starting from n/2 to 1, and the range of these
+    #  numbers is limited by the number of odd numbers from 1 to k, including k
   def add_regular_edges(self, degree):
-    pass
+    vList = [v for v in self]
+    if degree <= 0:
+      raise ValueError('degree must be greater than zero')
+    if degree > len(vList):
+      raise ValueError('degree must be less than number of vertices')
+    if len(vList)%2 != 0 and degree%2!=0:
+      raise ValueError('if the number of vertices is odd the degree must be even')
+
+    def add_edges(vertices, distances):
+      adjacentVertices = []
+      for d in distances:
+        for i in range(0, len(vertices), d):
+          adjacentVertices.append(vertices[i])
+      print(adjacentVertices)
+
+    if len(vList)%2 != 0:
+      x = [i for i in range(1, degree+1) if i%2!=0]
+      add_edges(vList, x)
+    elif len(vList)%2 == 0:
+      if degree%2 == 0:
+        x = set([i for i in range(1, int(len(vList)/2))])
+        x = x & set([i for i in range(1, degree+1) if i%2==0])
+        add_edges(vList, x)
+      elif degree%2 != 0:
+        x = set([i for i in range(int(len(vList)/2), 0, -1)])
+        x = x & set([i for i in range(1, degree+1) if i%2!=0])
+        add_edges(vList, x)
+    
+    
+    
