dcd3c4acd261027f217b2bd099661170931f1725
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-19 18:50:59 -0800

    RadomGraph.py class to create random graphs -- Erdős-Rényi random graph model

--- RandomGraph.py
+++ RandomGraph.py
@@ -1,5 +1,6 @@
-import random
-import itertools
+import random as rand
+import itertools as it
+import numpy as np
 
 from Graph import Graph
 from Vertex import Vertex
@@ -13,8 +14,14 @@ class RandomGraph(Graph):
     Graph.__init__(self, vs, es)
 
   def add_random_edges(self, probability):
-    combos = itertools.combinations(self.vertices(), 2)
-    print([i for i in combos])
+    # possible nodes combinations
+    combos = list(it.combinations(self.vertices(), 2))
+    # 1 - add edge, 0 - no edge added -- draw samples from binomial distribution
+    edge_hits = np.random.binomial(1, probability, len(combos))
+    # list of edges to be made
+    edges = [combos[i] for i in range(len(edge_hits)) if edge_hits[i] == 1]
+    for i in edges:
+      self.add_edge(Edge(i[0], i[1]))
 
 if __name__ == '__main__':
   v = Vertex('v')
@@ -22,5 +29,6 @@ if __name__ == '__main__':
   x = Vertex('x')
   y = Vertex('y')
   r = RandomGraph([v, w, x, y])
-  randomProbability = random.uniform(0, 1)
+  randomProbability = rand.uniform(0, 1)
   r.add_random_edges(randomProbability)
+  print(r.edges())
