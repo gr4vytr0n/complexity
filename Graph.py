692848de1b607422f707791d93912566bbcb3881
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-20 17:09:19 -0800

    working on is_connected method

--- Graph.py
+++ Graph.py
@@ -110,3 +110,44 @@ class Graph(dict):
         # distances are first numberOfOdds out of possibles
         distances = possibles[:numberOfOdds]
         add_edges(vList, distances)
+
+  # Determine if a graph is a `connected graph`
+  # Using breadth-first search
+  def is_connected(self):
+
+    # code from wikipedia to be adapted for use here
+    def bfs(problem):
+      # FIFO set
+      open_set = Queue()
+      # set of empty nodes
+      closed_set = set()
+      # meta info. (used for path formation)
+      meta = dict() # key -> (parent state, action to reach child)
+      # initialize
+      start = problem.get_start_state()
+      meta[start] = (None, None)
+      open_set.enqueue(start)
+
+      while not open_set.is_empty():
+        parent_state = open_set.dequeue()
+        if problem.is_goal(parent_state)
+          return construct_path(parent_state, meta)
+        for (child_state, action) in problem.get_successors(parent_state):
+          if child_state in closed_set:
+            continue
+          if child_state not in open_set:
+            meta[child_state] = (parent_state, action)
+            open_set.enqueue(child_state)
+
+        closed_set.add(parent_state)
+    def construct_path(state, meta):
+      action_list = list()
+      while True:
+        row = meta[state]
+        if len(row) == 2:
+          state = row[0]
+          action = row[1]
+          action_list.append(action)
+        else:
+          break
+      return action_list.reverse()
