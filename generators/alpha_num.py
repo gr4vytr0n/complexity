fb18b0410ec2e0b69ec2c927679a3ba4fbb339c8
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-12-01 08:45:41 -0800

    work on alpha_num.py

---
+++ generators/alpha_num.py
@@ -0,0 +1,14 @@
+'''
+    Genenerate alpha-numeric pairs (e.g. a1...z1, a2...z2, ...)
+'''
+from string import ascii_lowercase
+
+def alpha_num():
+    while True:
+        for l in ascii_lowercase:
+            for i in range(len(ascii_lowercase)):
+                yield l+str(i)
+
+if __name__ == '__main__':
+    for an in alpha_num():
+        print(an)
