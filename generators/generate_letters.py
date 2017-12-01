fb19a0cef3eb34de067580a97a577213ce70ba76
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-30 21:20:57 -0800

    a little bit of generator code

---
+++ generators/generate_letters.py
@@ -0,0 +1,17 @@
+'''
+    Generator: a function that contains a yield statement
+        -- makes an iterator
+'''
+from string import ascii_lowercase
+
+def generate_letters():
+    '''
+        next() -- returns next iteration of lowercase letter sequence
+    '''
+    for letter in ascii_lowercase:
+        yield letter
+
+if __name__ == '__main__':
+    g = generate_letters()
+    for i in g:
+        print(i)
